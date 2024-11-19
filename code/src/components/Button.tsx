921ca21864c12f6254f3201bee71c160c91eb235
Tue Do <tuedo.lm@gmail.com>
2024-11-19 20:46:19 +0700

    Finish NavBar section

--- code/src/components/Button.tsx
+++ code/src/components/Button.tsx
@@ -1,14 +1,17 @@
 import { HTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
-const classes = cva("border  h-12 rounded-full px-6 font-medium", {
-    variants: {
-        variant: {
-            primary: "bg-lime-400 text-neutral-950 border-lime-400",
-            secondary: "border-white text-white bg-transparent",
+const classes = cva(
+    "border h-12 rounded-full px-6 font-medium inline-flex items-center",
+    {
+        variants: {
+            variant: {
+                primary: "bg-lime-400 text-neutral-950 border-lime-400",
+                secondary: "border-white text-white bg-transparent",
+            },
         },
-    },
-});
+    }
+);
 
 export default function Button(
     props: {
--- code/src/sections/Navbar.tsx
+++ code/src/sections/Navbar.tsx
@@ -11,15 +11,24 @@ const navLinks = [
 
 export default function Navbar() {
     return (
-        <section className="py-4">
-            <div className="container">
-                <div className="grid grid-cols-2 border border-white/15 rounded-full px-4 md:pr-2 items-center">
+        <section className="py-4 lg:py-8">
+            <div className="container max-w-5xl">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 md:pr-2 items-center">
                     <div>
                         <Image
                             src={logoImage}
                             alt="logo"
-                            className="h-9 md:h-auto w-auto"
+                            className="h-9 ml-2 md:h-auto w-auto"
                         />
+                    </div>
+                    <div className="lg:flex justify-center items-center hidden">
+                        <nav className="flex gap-6 font-medium">
+                            {navLinks.map((link) => (
+                                <a key={link.href} href={link.href}>
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
                     </div>
                     <div className="flex justify-end gap-4">
                         <svg
@@ -32,14 +41,24 @@ export default function Navbar() {
                             strokeWidth="2"
                             strokeLinecap="round"
                             strokeLinejoin="round"
-                            className="lucide lucide-menu md:hidden"
+                            className="lucide lucide-menu mr-2 md:hidden"
                         >
                             <line x1="3" y1="12" x2="21" y2="12"></line>
                             <line x1="3" y1="6" x2="21" y2="6"></line>
                             <line x1="3" y1="18" x2="21" y2="18"></line>
                         </svg>
-                        <Button variant="secondary">Login</Button>
-                        <Button variant="primary">Sign Up</Button>
+                        <Button
+                            variant="secondary"
+                            className="hidden md:inline-flex"
+                        >
+                            Log In
+                        </Button>
+                        <Button
+                            variant="primary"
+                            className="hidden md:inline-flex"
+                        >
+                            Sign Up
+                        </Button>
                     </div>
                 </div>
             </div>
