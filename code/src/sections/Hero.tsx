6380bf67457b2a5b40aab83efe9b8f87599e1b9a
Tue Do <tuedo.lm@gmail.com>
2024-11-26 00:33:47 +0700

    add Pointer.tsx and finish Hero Section ( need review on mobile size )

--- code/src/sections/Hero.tsx
+++ code/src/sections/Hero.tsx
@@ -1,27 +1,51 @@
+import Image from "next/image";
 import Button from "@/components/Button";
+import Pointer from "@/components/Pointer";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
 
 export default function Hero() {
     return (
-        <section className="py-24">
-            <div className="container">
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div>
+                    <Image
+                        src={designExample1Image}
+                        alt="Design Example 1"
+                        className="absolute -left-32 top-16 hidden lg:block"
+                    />
+                </div>
+                <div>
+                    <Image
+                        src={designExample2Image}
+                        alt="Design Example 1"
+                        className="absolute -right-64 -top-16 hidden lg:block"
+                    />
+                </div>
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name="Andrea" />
+                </div>
+                <div className="absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name="Bryan" color="red" />
+                </div>
                 <div className="flex justify-center">
-                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
-                        ✨ $7.5M seed ound raised
+                    <div className="inline-flex py-1 px-2 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-bold">
+                        ✨ $7.5M seed round raised
                     </div>
                 </div>
-                <h1 className="text-6xl font-medium text-center mt-6">
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
                     Impactful design, created effortlessly
                 </h1>
-                <p className="text-center text-xl text-white/50 mt-8">
-                    Design tools shouldn&apos;t slow you down. Layers combine
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+                    Design tools shouldn&apos;t slow you down. Layers combines
                     powerful features with an intuitive interface that keeps you
                     in your creative flow.
                 </p>
-                <form className="flex border border-white/15 rounded-full p-2 mt-8">
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
                     <input
                         type="email"
                         placeholder="Enter your email"
-                        className="bg-transparent px-4 placeholder:text-white/50"
+                        className="bg-transparent px-4 md:flex-1"
                     />
                     <Button
                         type="submit"
