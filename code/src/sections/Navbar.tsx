e4b0975202fe992ba6859b03c8d9d6504b45400b
Tue Do <tuedo.lm@gmail.com>
2024-11-28 01:12:07 +0700

    finish burger menu animation in NavBar

--- code/src/sections/Navbar.tsx
+++ code/src/sections/Navbar.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import logoImage from "../assets/images/logo.svg";
 import Button from "../components/Button";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -10,10 +14,11 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <section className="py-4 lg:py-8">
+        <section className="py-4 lg:py-8 sticky top-0 z-50">
             <div className="container max-w-5xl">
-                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 md:pr-2 items-center">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 md:pr-2 items-center bg-neutral-950/70 backdrop-blur">
                     <div>
                         <Image
                             src={logoImage}
@@ -42,10 +47,11 @@ export default function Navbar() {
                             strokeLinecap="round"
                             strokeLinejoin="round"
                             className="lucide lucide-menu mr-2 md:hidden"
+                            onClick={() => setIsOpen(!isOpen)}
                         >
-                            <line x1="3" y1="12" x2="21" y2="12"></line>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <line x1="3" y1="18" x2="21" y2="18"></line>
+                            <line x1="3" y1="6" x2="21" y2="6" className={twMerge("origin-left transition",isOpen && "rotate-45 -translate-y-1")}></line>
+                            <line x1="3" y1="12" x2="21" y2="12" className={twMerge("transition",isOpen && "opacity-0")}></line>
+                            <line x1="3" y1="18" x2="21" y2="18" className={twMerge("origin-left transition",isOpen && "-rotate-45 translate-y-1")}></line>
                         </svg>
                         <Button
                             variant="secondary"
