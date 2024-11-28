3ec48d71ec6770870c1f5b7721814bbd8804c3e4
Tue Do <tuedo.lm@gmail.com>
2024-11-29 00:12:19 +0700

    finished the CTA section and the whole project.

--- code/src/sections/CallToAction.tsx
+++ code/src/sections/CallToAction.tsx
@@ -1,15 +1,55 @@
+"use client";
+
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 export default function CallToAction() {
-    return(
+    const [isHovered, setIsHovered] = useState(false);
+    const animation = useRef<AnimationPlaybackControls>();
+    const [scope, animate] = useAnimate();
+
+    useEffect(() => {
+        animation.current = animate(
+            scope.current,
+            {
+                x: "-50%",
+            },
+            {
+                duration: 30,
+                repeat: Infinity,
+                ease: "linear",
+            }
+        );
+    }, []);
+
+    useEffect(() => {
+        if (animation.current) {
+            if (isHovered) {
+                animation.current.speed = 0.5;
+            } else {
+                animation.current.speed = 1;
+            }
+        }
+    }, [isHovered]);
+    return (
         <section className="py-24">
             <div className="overflow-x-clip p-4 flex">
-                <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
-                    {Array.from({length: 10}).map((_, index) => (
+                <motion.div
+                    ref={scope}
+                    className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium group cursor-pointer"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    {Array.from({ length: 10 }).map((_, index) => (
                         <div key={index} className="flex items-center gap-16">
-                            <span className="text-lime-400 text-7xl">&#10038;</span>
-                            <span>Try it for free</span>
+                            <span className="text-lime-400 text-7xl">
+                                &#10038;
+                            </span>
+                            <span className="group-hover:text-lime-400">
+                                Try it for free
+                            </span>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
     );
